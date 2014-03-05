e3b80f2f84498d5b92c103b1ef2a884f38b2fa0a
Garrett Johnson <garrett@recurly.com>
2014-03-05 11:13:09 -0500

    Add ability to get the current time

--- test/index.js
+++ test/index.js
@@ -4,9 +4,17 @@ var unixTime = require('..')
 
 
 describe('unix-time', function () {
+  describe('when a date argument is given', function () {
+    it('should convert it to a unix timestamp', function () {
+      var date = new Date(1374016861934);
+      unixTime(date).should.eql(1374016861);
+    });
+  });
 
-  it('should convert a date to a unix timestamp', function () {
-    var date = new Date(1374016861934);
-    unixTime(date).should.eql(1374016861);
+  describe('when no arguments are given', function () {
+    it('should return the current timestamp', function () {
+      var expected = Math.floor(new Date().getTime() / 1000);
+      unixTime().should.equal(expected);
+    });
   });
 });
